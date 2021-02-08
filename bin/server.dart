21021a2d6f121cd3e4e7c29abf432f1de428dae6
Maykon Taroco <maykontaroco@gmail.com>
2021-02-08 17:55:09 -0300

    Initial bakcend configuration

--- bin/server.dart
+++ bin/server.dart
@@ -1,8 +1,12 @@
 import 'dart:io';
 
 import 'package:args/args.dart';
+import 'package:dart_week/aplication/config/database_connection_configuration.dart';
+import 'package:dart_week/aplication/config/service_locator_config.dart';
+import 'package:get_it/get_it.dart';
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
+import 'package:dotenv/dotenv.dart' show load, env;
 
 // For Google Cloud Run, set _hostname to '0.0.0.0'.
 const _hostname = 'localhost';
@@ -32,3 +36,20 @@ void main(List<String> args) async {
 
 shelf.Response _echoRequest(shelf.Request request) =>
     shelf.Response.ok('Request for "${request.url}"');
+
+
+Future<void> loadConfigApplication() async {
+  await load();
+
+  final databaseConfig = DatabaseConnectionConfiguration(
+    host: Platform.environment['DATABASE_HOST'] ?? env['databaseHost'],
+    user: Platform.environment['DATABASE_USER'] ?? env['databaseUser'],
+    password:
+    Platform.environment['DATABASE_PASSWORD'] ?? env['databasePassword'],
+    port: Platform.environment['DATABASE_PORT'] ?? env['databasePort'],
+    databaseName: Platform.environment['DATABASE_NAME'] ?? env['databaseName'],
+  );
+
+  GetIt.I.registerSingleton(databaseConfig);
+  configureDependencies();
+}
